bc8ee5823778b78cda9509234317b36e2b998116
cjh-19 <gns8812@naver.com>
2025-02-02 00:23:29 +0900

    feat: dto 설정

---
+++ src/main/java/com/crud/crudpractice/movie/dto/MoviePosterDto.java
@@ -0,0 +1,13 @@
+package com.crud.crudpractice.movie.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class MoviePosterDto {
+    private Long posterId;
+    private Long movieId;
+    private String posterUrl;
+    private LocalDateTime createdAt;
+}
